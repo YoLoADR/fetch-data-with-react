d90b7c5a8cd48bd555ed66abb4d8e2151bf5a2d0
YoLoADR <YohannPro@macbook-air-de-yohann.home>
2019-07-15 11:46:23 +0200

    recuperation de donnee via une API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+	state = {
+		loadind: true,
+		person: null
+	}
+
+	async componentDidMount() {
+		const url = 'https://api.randomuser.me/'
+
+		const response = await fetch(url)
+		const data = await response.json()
+		this.setState({ person: data.results[0], loadind: false })
+		console.log('data', data.results[0])
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Fetch data with ReactJs</h1>
+				{this.state.loadind || !this.state.person ? (
+					<div>loading...</div>
+				) : (
+					<div>
+						<div>{this.state.person.name.first}</div>
+						<div>{this.state.person.name.last}</div>
+						<img src={this.state.person.picture.large} alt="" />
+					</div>
+				)}
+			</div>
+		)
+	}
 }
-
-export default App;
